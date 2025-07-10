d09b9492b2a723b13b42166f179ce52073238786
CJ28M <marin.christopher128@gmail.com>
2025-07-09 22:27:04 -0700

    lesson 17: New card button

--- 3. Build a Blackjack game/15. Display the sum/index.js
+++ 3. Build a Blackjack game/15. Display the sum/index.js
@@ -1,22 +1,36 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
-let messageEl = document.getElementById("message-el")
+let firstCard = 10;
+let secondCard = 4;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
 // 2. Store the sum paragraph in a variable called sumEl
+let sumEl = document.getElementById("sum-el");
+// let sumEl = document.querySelector("#sum-el");
+// let sumResult = "";
 
 function startGame() {
-    // 3. Render the sum on the page using this format -> "Sum: 14"
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game!"
-        isAlive = false
-    }
-    messageEl.textContent = message
+  // 3. Render the sum on the page using this format -> "Sum: 14"
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+    console.log(sum);
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+  sumEl.textContent = "Sum: " + sum;
+}
+
+{
+  /* <h1>Blackjack</h1>
+<p id="message-el">Want to play a round?</p>
+<p>Cards:</p>
+<!-- 1. Give the sum paragraph and id of sum-el -->
+<p id="sum-el">Sum:</p>
+<button onclick="startGame()">START GAME</button> */
 }
