bb77fca93bce4458e3f47b2aa27a4888d9412e0c
CJ28M <marin.christopher128@gmail.com>
2025-07-11 16:56:16 -0700

    L48 only trigger newCard() if you're allowed to

--- 3. Build a Blackjack game/46. Write your first logical operator/index.js
+++ 3. Build a Blackjack game/46. Write your first logical operator/index.js
@@ -1,11 +1,12 @@
-let hasSolvedChallenge = false
-let hasHintsLeft = false
+let hasSolvedChallenge = false;
+let hasHintsLeft = false;
 
 // Create an if statement that checks that both variables are false.
+if (hasSolvedChallenge === false && hasHintsLeft === false) {
+  showSolution();
+}
 // If so, run the showSolution() function
 
 function showSolution() {
-    console.log("Showing the solution....")
+  console.log("Showing the solution....");
 }
-
-
