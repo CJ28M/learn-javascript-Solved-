8f635e157005c26526ec080fa1d449418ea8c605
CJ28M <marin.christopher128@gmail.com>
2025-07-11 12:26:22 -0700

    lesson 37: Math.random() * 6

--- 3. Build a Blackjack game/36. Generating random numbers with Math.random()/index.js
+++ 3. Build a Blackjack game/36. Generating random numbers with Math.random()/index.js
@@ -1,12 +1,12 @@
-let randomNumber = Math.random()
+let randomNumber = Math.random();
 
-console.log(randomNumber)
-
+console.log(randomNumber);
 
 /* 
 
 What does Math.random() do?
 
-Your answer: 
+Your answer: it generates a random number between 0 and 1 (not inclusive of 1)
+.0000... - .999...
 
 */
