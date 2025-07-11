f0577291ffc5eba3abc4650211d01359aeccd29c
CJ28M <marin.christopher128@gmail.com>
2025-07-10 19:22:25 -0700

    lesson 32: use a loop to render cards

--- 3. Build a Blackjack game/28. Write your first loop/index.js
+++ 3. Build a Blackjack game/28. Write your first loop/index.js
@@ -1,4 +1,10 @@
-
+for (let x = 0; x < 6; x += 1) {
+  // console.log(x); // 0,1,2,3,4,5
+}
 
 // Create a for loop that counts from 10 to 100 in steps of 10
 // Use console.log to log out the numbers
+
+for (let n = 10; n < 101; n += 10) {
+  console.log(n);
+}
--- 3. Build a Blackjack game/30. Write you first array-based for loop/index.js
+++ 3. Build a Blackjack game/30. Write you first array-based for loop/index.js
@@ -1,6 +1,8 @@
-let cards = [7, 3, 9]
+let cards = [7, 3, 9];
 
 // Create a for loop that logs out all the cards in the array
 // Use cards.length to specify how long the loop should run
 
-
+for (let c = 0; c < cards.length; c++) {
+  console.log(cards[c]);
+}
