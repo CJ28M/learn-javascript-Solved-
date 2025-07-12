8bc4a0285df4b9e758bff0bdd15d0cca8ef87223
CJ28M <marin.christopher128@gmail.com>
2025-07-12 11:45:52 -0700

    L51 use an object to store player data

--- 3. Build a Blackjack game/51. Create your first object/index.js
+++ 3. Build a Blackjack game/51. Create your first object/index.js
@@ -2,4 +2,11 @@
 // It should contain at least one boolean, one string, one number, and one array
 // Log out at least two of the keys using the dot notation
 
+let castle = {
+  big: true,
+  location: "london",
+  year: 1778,
+  images: ["img/castle1.png", "img/castle2.png"],
+};
 
+console.log(castle.year + castle.big);
