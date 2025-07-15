97d0eac3b36b90a40430edf1dfc18e2bd2b3a12f
CJ28M <marin.christopher128@gmail.com>
2025-07-15 09:38:07 -0700

    L28 refactor the app to use a template string

--- 5. Build a Chrome Extension/27. Template strings on multiple lines/index.js
+++ 5. Build a Chrome Extension/27. Template strings on multiple lines/index.js
@@ -1,9 +1,10 @@
 // template strings/literals
 
-const recipient = "James"
-const sender = "Per Harald Borgen"
+const recipient = "James";
+const sender = "Per Harald Borgen";
 
 // Break the email string into multiple lines
-const email = `Hey ${recipient}! How is it going? Cheers ${sender}`
-
-console.log(email)
+const email = `Hey ${recipient}!
+How is it going? 
+Cheers ${sender}`;
+console.log(email);
