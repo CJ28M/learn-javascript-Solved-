2415be2cf12c39f35f82b576c82789d087c45ed9
CJ28M <marin.christopher128@gmail.com>
2025-07-15 20:06:15 -0700

    L39 checking ls before rendering

--- 5. Build a Chrome Extension/36. Get the leads from localStorage/index.js
+++ 5. Build a Chrome Extension/36. Get the leads from localStorage/index.js
@@ -1,32 +1,37 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
-// Get the leads from the localStorage
+// Get the leads from the localStorage-PS: JSON.parse()
+// localStorage.clear();
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+console.log(leadsFromLocalStorage);
 // Store it in a variable, leadsFromLocalStorage
 // Log out the variable
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    renderLeads()
-    
-    // To verify that it works:
-    console.log( localStorage.getItem("myLeads") )
-})
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  // Save the myLeads array to localStorage
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  renderLeads();
+
+  // To verify that it works:
+  console.log(localStorage.getItem("myLeads"));
+});
 
 function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
             <li>
                 <a target='_blank' href='${myLeads[i]}'>
                     ${myLeads[i]}
                 </a>
             </li>
-        `
-    }
-    ulEl.innerHTML = listItems  
+        `;
+  }
+  ulEl.innerHTML = listItems;
 }
--- 5. Build a Chrome Extension/38. Truthy and falsy values/index.js
+++ 5. Build a Chrome Extension/38. Truthy and falsy values/index.js
@@ -1,6 +1,9 @@
-console.log(  Boolean("")   ) //
-console.log(  Boolean("0")  ) //
-console.log(  Boolean(100)  ) //
-console.log(  Boolean(null) ) //
-console.log(  Boolean([0])  ) //
-console.log(  Boolean(-0)   ) //
+console.log(Boolean("")); //f
+console.log(Boolean("0")); //
+console.log(Boolean(100)); //
+console.log(Boolean(null)); //
+console.log(Boolean([0])); //
+console.log(Boolean(-0)); //
+
+let trueOrFalse = Boolean("");
+console.log(trueOrFalse);
